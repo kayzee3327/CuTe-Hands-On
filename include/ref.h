f7547689bc2741b98a5a9164979e2c7e37eabb6c
王楷泽 <kayzee3327@163.com>
2026-04-12 22:02:02 +0800

    re-organize the project (only examples now)

--- include/ref.h
+++ include/ref.h
@@ -13,34 +13,6 @@
 #include <cublas_v2.h>
 
 #include <iostream>
-#include <utility>
-#include <concepts> // For std::invocable
-#include <type_traits> // For std::is_same_v and std::invoke_result_t
-
-// check cuda apis
-#define CUDA_CHECK(status)                                              \
-    {                                                                   \
-    cudaError_t error = status;                                         \
-    if (error != cudaSuccess) {                                         \
-        std::cerr << "CUDA Error: " << cudaGetErrorString(error) << " in "\
-                  << __func__ << " at "                                 \
-                  << __FILE__ << ":" << __LINE__ << std::endl;          \
-        exit(EXIT_FAILURE);                                             \
-    }                                                                   \
-    }
-// check kernel launch
-#define CUDA_CHECK_LAST_ERROR()                                                 \
-    do {                                                                        \
-        cudaError_t error = cudaGetLastError();                                 \
-        if (error != cudaSuccess) {                                             \
-            std::cerr << "CUDA Error: " << cudaGetErrorString(error) << " in "  \
-                      << __func__ << " at "                                     \
-                      << __FILE__ << ":" << __LINE__ << std::endl;              \
-            exit(EXIT_FAILURE);                                                 \
-        }                                                                       \
-    } while (0)
-
-#define LAMBDA_WRAPPER(func) [](auto&&... fw_args){return func(std::forward<decltype(fw_args)>(fw_args)...);}
 
 // Raw-pointer ref_gemm using cuBLAS (row-major, all matrices contiguous).
 // transA/transB describe the math operation on A and B:
@@ -147,70 +119,3 @@ void tensor_cmp(cutlass::HostTensor<TA, LayoutA>& result,
         // Optional: use cutlass::reference::host::TensorRelativeAndAbsoluteError to debug
     }
 }
-
-
-// template<typename Callable, typename... Args>
-// requires std::invocable<Callable, Args...>
-// bool is_cuda_kernel(Callable f)
-// {
-//     if constexpr (std::is_pointer_v<Callable>) {
-//         cudaFuncAttribute attr;
-//         cudaError_t err = cudaFuncGetAttributes(&attr, f);
-//         if (err == cudaSuccess)
-//         {
-//             return true;
-//         }
-//         cudaGetLastError();
-//     }
-//     return false;
-// }
-
-
-// template<typename Callable, typename... Args>
-// requires std::invocable<Callable, Args...>
-// auto profile(int iters, double flop, Callable func, Args&&... args) 
-// {
-//     using RetType = std::invoke_result_t<Callable, Args...>;
-    
-//     GPU_Clock timer;
-//     double tflops = flop * 1e-12;
-//     if constexpr (std::is_same_v<RetType, void>)
-//     {
-//         // warm up caches
-//         func(std::forward<Args>(args)...);
-
-//         timer.start();
-//         for (int i = 0; i < iters; i++)
-//         {
-//             func(std::forward<Args>(args)...);
-//         }
-//         double time = timer.seconds() / iters;
-//         CUDA_CHECK_LAST_ERROR();
-//         printf("Profiling results:    [%f]TFlop/s  (%f)ms\n", 
-//                 tflops / time, time*1000);
-//         return;
-//     }
-//     else
-//     {
-//         // warm up caches
-//         func(std::forward<Args>(args)...);
-        
-//         std::vector<RetType> results;
-//         results.reserve(iters);
-        
-//         timer.start();
-//         for (int i = 0; i < iters; i++)
-//         {
-//             results.push_back(func(std::forward<Args>(args)...));
-//         }
-//         double time = timer.seconds() / iters;
-//         CUDA_CHECK_LAST_ERROR();
-//         printf("Profiling results:    [%f]TFlop/s  (%f)ms\n", 
-//                 tflops / time, time*1000);
-//         return results;
-            
-//     }
-
-    
-    
-// }
